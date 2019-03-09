7ffb44ae35b2d23af0ae0f9484adab8d938a868f
Erin McIntyre <43821984+smartscrutiny@users.noreply.github.com>
2019-03-09 12:31:22 -0500

    Update jquery-change-div-class-dynamically.js

--- jquery-change-div-class-dynamically.js
+++ jquery-change-div-class-dynamically.js
@@ -2,7 +2,7 @@
 ------------------------------
 Bootstrap's grid handles most browser resize issues beautifully, but I ran into a situation 
 where I really needed a div with a col-md-9 class to change dynamically to a div with a col-md-12 class at certain
-browser sizes only. I gave these divs an id of #jquery-target. This jQuery snippet did the trick.
+browser sizes only. I gave these divs an id of #jquery-target. This jQuery snippet did the trick. See it in action at http://jsfiddle.net/smartscrutiny/b3q0gwds/10/.
 ------------------------------
 */
 
