3d91b66e067bca08e0e6588a6bd4a95d0949b9bf
Erin McIntyre <43821984+smartscrutiny@users.noreply.github.com>
2019-03-09 14:22:01 -0500

    Update jquery-make-div-same-width-as-another-div.js

--- jquery-make-div-same-width-as-another-div.js
+++ jquery-make-div-same-width-as-another-div.js
@@ -4,6 +4,7 @@ Sometimes you want one div to change its width based on the width of another div
 If the divs are changing their dimensions based on the browser size, this can be tricky!
 Here's a way to do it with jQuery. #div2ID is the div you want to resize, based on the
 width of #div1ID. The same can be done with div height.
+See it in action at https://jsfiddle.net/smartscrutiny/scbuy4hv/18/.
 ------------------------------
 */
 
